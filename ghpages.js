329ad3c0310e2f75f60bace0fcab1bd575dd6816
Alexander Kurganow <me@akurganow.ru>
2018-05-03 15:32:00 +0300

    feat(deploy): add deploy script

---
+++ ghpages.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+const ghpages = require('gh-pages')
+
+console.info('Start deploy to gh-pages')
+
+ghpages.publish(
+  'public',
+  {
+    user: {
+      name: 'Alex Kurganov',
+      email: 'me@akurganow.ru',
+    },
+    repo:
+      'https://' +
+      process.env.GH_TOKEN +
+      '@github.com/spb-frontend/spb-frontend-content.git',
+    message: 'Updates [ci skip]',
+  },
+  () => {
+    console.info('Deploy finished')
+  },
+)
